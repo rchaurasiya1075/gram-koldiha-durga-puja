18253c1be57e9106ce55931c45837c3887f5dc28
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-07 23:43:42 +0530

    Ticker uses कोल्डीहा पोस्ट लिलवाही; load addr-ui v=39

--- firebase-config.js
+++ firebase-config.js
@@ -13,6 +13,6 @@ window.FIREBASE_READY = function () {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js").catch(function () {});
 }
-["addr-ui.js?v=37","live-sync.js?v=38"].forEach(function(src){
+["addr-ui.js?v=39","live-sync.js?v=38"].forEach(function(src){
   var s=document.createElement("script");s.src="./"+src;document.head.appendChild(s);
 });

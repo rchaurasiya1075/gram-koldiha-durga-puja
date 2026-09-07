2b650314953f675c1ad80416711fa3d0c2aad5a3
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-07 22:03:36 +0530

    Load admin-users.js

--- boot-social.js
+++ boot-social.js
@@ -1,5 +1,5 @@
 (function(){
-  ["gal-perm.js?v=22","social.js?v=24","chat-ui.js?v=25","notify.js?v=26","fest.js?v=27","notify-extra.js?v=27","welcome.js?v=28"].forEach(function(src){
+  ["gal-perm.js?v=22","social.js?v=24","chat-ui.js?v=25","notify.js?v=26","fest.js?v=27","notify-extra.js?v=27","welcome.js?v=28","admin-users.js?v=29"].forEach(function(src){
     var s=document.createElement("script");s.src="./"+src;document.body.appendChild(s);
   });
 })();

7aee07c44f628122316833373c4af3ba2b022511
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-07 21:02:00 +0530

    Load social.js with gal-perm

---
+++ boot-social.js
@@ -0,0 +1,7 @@
+(function(){
+  ["gal-perm.js?v=22","social.js?v=23"].forEach(function(src){
+    var s=document.createElement("script");
+    s.src="./"+src;
+    document.body.appendChild(s);
+  });
+})();

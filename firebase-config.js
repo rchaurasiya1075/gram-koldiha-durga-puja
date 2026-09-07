1cfe21d18a1da99b2a57354cc5f3d467f2cd7873
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-07 23:39:30 +0530

    Auto-load live-sync.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,8 +13,6 @@ window.FIREBASE_READY = function () {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js").catch(function () {});
 }
-(function(){
-  var s=document.createElement("script");
-  s.src="./addr-ui.js?v=37";
-  document.head.appendChild(s);
-})();
+["addr-ui.js?v=37","live-sync.js?v=38"].forEach(function(src){
+  var s=document.createElement("script");s.src="./"+src;document.head.appendChild(s);
+});

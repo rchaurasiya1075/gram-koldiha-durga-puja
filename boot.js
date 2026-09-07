937e8baa244f9d93268b42b682076552186d550c
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-07 20:41:49 +0530

    Load gal-perm.js

--- boot.js
+++ boot.js
@@ -1,7 +1,5 @@
 (function(){
-  ["donate.js?v=19","access.js?v=19","share.js?v=19"].forEach(function(src){
-    var s=document.createElement("script");
-    s.src="./"+src;
-    document.body.appendChild(s);
-  });
+  var s=document.createElement("script");
+  s.src="./gal-perm.js?v=21";
+  document.body.appendChild(s);
 })();

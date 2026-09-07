dc04df769c61a1e78cc95d9448edd2dacb3d8ec5
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-07 21:11:52 +0530

    Force-load latest social chat UI

--- boot-social.js
+++ boot-social.js
@@ -1,7 +1,5 @@
 (function(){
-  ["gal-perm.js?v=22","social.js?v=23"].forEach(function(src){
-    var s=document.createElement("script");
-    s.src="./"+src;
-    document.body.appendChild(s);
-  });
+  var s=document.createElement("script");
+  s.src="./social.js?v=24";
+  document.body.appendChild(s);
 })();

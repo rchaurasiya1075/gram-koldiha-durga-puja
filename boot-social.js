0bb01fd0c0c2cca06323bae784126ba1ce1a58aa
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-07 21:20:10 +0530

    Load notify.js v=26

--- boot-social.js
+++ boot-social.js
@@ -1,5 +1,7 @@
 (function(){
-  var s=document.createElement("script");
-  s.src="./social.js?v=24";
-  document.body.appendChild(s);
+  ["gal-perm.js?v=22","social.js?v=24","chat-ui.js?v=25","notify.js?v=26"].forEach(function(src){
+    var s=document.createElement("script");
+    s.src="./"+src;
+    document.body.appendChild(s);
+  });
 })();

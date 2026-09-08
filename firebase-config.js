88f3235ab6824b1771f238c80a5b566ba0cfb5d3
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-09-08 14:58:50 +0530

    Load voice.js v=42

--- firebase-config.js
+++ firebase-config.js
@@ -13,6 +13,6 @@ window.FIREBASE_READY = function () {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js").catch(function () {});
 }
-["addr-ui.js?v=40","live-sync.js?v=38","voice.js?v=41"].forEach(function(src){
+["addr-ui.js?v=40","live-sync.js?v=38","voice.js?v=42"].forEach(function(src){
   var s=document.createElement("script");s.src="./"+src;document.head.appendChild(s);
 });
